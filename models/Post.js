fdf6386c3b01b44749ca9715db415890924ec393
arestch <invfalle@gmail.com>
2018-06-22 10:37:40 +0300

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const moongose = require("mongoose");
+const Schema = moongose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: date.now()
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: date.now()
+  }
+});
+
+module.exports = Post = moongose.model("posts", PostSchema);
