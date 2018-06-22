17981bd035ec4f61b641d047e870833531577303
arestch <invfalle@gmail.com>
2018-06-22 12:46:47 +0300

    added api for like postse

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -65,27 +65,32 @@ router.get("/:id", (req, res) => {
     );
 });
 
-// @route   DELETE api/post/:id
-// @desc    Get post by id
+// @route   POST api/post/like/:id
+// @desc    Like post
 // @access  Private
-router.delete(
-  "/:id",
+router.post(
+  "/like/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Profile.findOne({ user: req.user.id }).then(profile => {
       Post.findById(req.params.id)
         .then(post => {
-          // Check for post owner
-          if (post.user.toString() !== req.user.id) {
-            return res
-              .status(401)
-              .json({ notauthorized: "User not authorized" });
-          }
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            // Remove like in case if user liked that post already
+            const removeIndex = post.likes
+              .map(like => like.user)
+              .indexOf(req.user.id);
 
-          // Delete
-          post.remove().then(() => {
-            res.json({ success: true });
-          });
+            post.likes.splice(removeIndex, 1);
+            post.save().then(post => res.json(post));
+          } else {
+            // Add user id to likes array
+            post.likes.unshift({ user: req.user.id });
+            post.save().then(post => res.json(post));
+          }
         })
         .catch(err => res.status(404).json({ postnotfound: "No post found" }));
     });
