c97726092453c6952adee09af5f7e683814d0af4
arestch <invfalle@gmail.com>
2018-06-22 11:52:21 +0300

    fixed post model & added most of api for posts

--- models/Post.js
+++ models/Post.js
@@ -43,13 +43,13 @@ const PostSchema = new Schema({
       },
       date: {
         type: Date,
-        default: date.now()
+        default: Date.now()
       }
     }
   ],
   date: {
     type: Date,
-    default: date.now()
+    default: Date.now()
   }
 });
 
