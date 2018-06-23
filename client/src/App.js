5d31c25efa4200caa32b224295665b243d1a3fc9
arestch <invfalle@gmail.com>
2018-06-23 09:22:40 +0300

    Added landing page, routes & created components for login/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> React APP </h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
