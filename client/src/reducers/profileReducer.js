adc595e2e490a2633d277b249953052de8e9c4b0
arestch <invfalle@gmail.com>
2018-06-24 11:01:45 +0300

    added profile reduce & actions for profile

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,36 @@
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE
+} from "../actions/types";
+import { Z_ASCII } from "zlib";
+
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: action.payload,
+        loading: false
+      };
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case CLEAR_CURRENT_PROFILE:
+      return {
+        ...state,
+        profile: null
+      };
+    default:
+      return state;
+  }
+}
