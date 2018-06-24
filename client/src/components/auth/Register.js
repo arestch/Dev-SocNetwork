aab6f3678817c0a9827da7ef73ff9ba04838f64e
arestch <invfalle@gmail.com>
2018-06-24 07:03:30 +0300

    User now cant go to register page if hes logged

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -29,6 +29,12 @@ class Register extends Component {
     this.setState({ [fieldName]: e.target.value, errors: newErrors });
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
