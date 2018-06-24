cefe7741268d168da91654c6623e326c08a5d2b3
arestch <invfalle@gmail.com>
2018-06-24 08:35:50 +0300

    footer is now sticky to the bottom

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default () => {
   return (
-    <div>
-      <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; {new Date().getFullYear()} Dev Connector
-      </footer>
-    </div>
+    <footer className="bg-dark text-white mt-5 p-4 text-center footer">
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
+    </footer>
   );
 };
