7c4bb338586376c04bfee658161de3aad3776bbc
arestch <invfalle@gmail.com>
2018-06-25 10:36:53 +0300

    added functionality for create profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -26,6 +26,18 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Profile Loading
 export const setProfileLoading = () => {
   return {
--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -5,6 +5,8 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
+import { withRouter } from "react-router-dom";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -34,6 +36,30 @@ class CreateProfile extends Component {
 
   onSubmit(e) {
     e.preventDefault();
+
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange(e) {
@@ -143,7 +169,7 @@ class CreateProfile extends Component {
                 />
                 <TextFieldGroup
                   placeholder="Website"
-                  value={this.state.handle}
+                  value={this.state.website}
                   name="website"
                   onChange={this.onChange}
                   error={errors.website}
@@ -175,20 +201,13 @@ class CreateProfile extends Component {
                 />
                 <TextAreaFieldGroup
                   placeholder="Short Bio"
-                  value={this.state.githubusername}
+                  value={this.state.bio}
                   name="bio"
                   onChange={this.onChange}
                   error={errors.bio}
                   info="Tell us a little about yourself"
                 />
-                <TextAreaFieldGroup
-                  placeholder="Short Bio"
-                  value={this.state.githubusername}
-                  name="bio"
-                  onChange={this.onChange}
-                  error={errors.bio}
-                  info="Tell us a little about yourself"
-                />
+
                 <div className="mb-3">
                   <button
                     onClick={() => {
@@ -227,4 +246,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(withRouter(CreateProfile));
