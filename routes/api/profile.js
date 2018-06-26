40366e3ef4eb17aa510f44a81f2b936529e58638
arestch <invfalle@gmail.com>
2018-06-26 08:36:49 +0300

    small api refactoring

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -105,50 +105,45 @@ router.post(
     }
 
     // Get fields
-    const profileFields = {};
-    profileFields.user = req.user.id;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-      profileFields.githubusername = req.body.githubusername;
+    const profileFields = Object.keys(req.body);
+    const newProfile = {};
+    profileFields.map(item => {
+      newProfile[item] = req.body[item];
+    });
     // Skills - Split into array
     if (typeof req.body.skills !== "undefined") {
-      profileFields.skills = req.body.skills.split(",");
+      newProfile.skills = req.body.skills.split(",");
     }
 
-    // Social
-    profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.handle) profileFields.handle = req.body.handle;
+    // Social Links
+    newProfile.social = {};
+    if (req.body.youtube) newProfile.social.youtube = req.body.youtube;
+    if (req.body.instagram) newProfile.social.instagram = req.body.instagram;
+    if (req.body.linkedin) newProfile.social.linkedin = req.body.linkedin;
+    if (req.body.facebook) newProfile.social.facebook = req.body.facebook;
+    if (req.body.twitter) newProfile.social.twitter = req.body.twitter;
+    if (req.body.handle) newProfile.handle = req.body.handle;
 
     Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         // Update
         Profile.findOneAndUpdate(
           { user: req.user.id },
-          { $set: profileFields },
+          { $set: newProfile },
           { new: true }
         ).then(profile => res.json(profile));
       } else {
         // Create
 
         // Check if handle exists
-        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+        Profile.findOne({ handle: newProfile.handle }).then(profile => {
           if (profile) {
             errors.handle = "That handle already exists";
             res.status(400).json(errors);
           }
 
           // Save Profile
-          new Profile(profileFields).save().then(profile => res.json(profile));
+          new Profile(newProfile).save().then(profile => res.json(profile));
         });
       }
     });
