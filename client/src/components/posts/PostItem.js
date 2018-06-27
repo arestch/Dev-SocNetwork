58567ca595d36841cfe84d505c81415c33e61301
arestch <invfalle@gmail.com>
2018-06-27 21:43:32 +0300

    small fixing

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -26,13 +26,11 @@ class PostItem extends Component {
       <div className="card card-body mb-3">
         <div className="row">
           <div className="col-md-2">
-            <a href="profile.html">
-              <img
-                className="rounded-circle d-none d-md-block"
-                src={post.avatar}
-                alt="Avatar"
-              />
-            </a>
+            <img
+              className="rounded-circle d-none d-md-block"
+              src={post.avatar}
+              alt="Avatar"
+            />
             <br />
             <p className="text-center">{post.name}</p>
           </div>
