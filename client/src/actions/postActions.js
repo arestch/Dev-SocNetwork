001e1ae45f6988ae0d71c14709a2ef615ee2152e
arestch <invfalle@gmail.com>
2018-06-27 17:32:17 +0300

    added post-feed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
+// Add post
 export const addPost = postData => dispatch => {
+  dispatch(getPosts);
   axios
     .post("/api/posts", postData)
     .then(res => {
       dispatch({
         type: ADD_POST,
         payload: res.data
-      });
-      dispatch({
-        type: GET_ERRORS,
-        payload: {}
       });
     })
     .catch(err =>
@@ -21,4 +19,28 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const getPosts = () => dispatch => {
+  axios
+    .get("/api/posts")
+    .then(res => {
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: {}
+      })
+    );
+};
+
+// Set Loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import PostForm from "./PostForm";
-import { Spinner } from "./../common/spinner";
+import PostFeed from "./PostFeed";
+import Spinner from "./../common/spinner";
+import { getPosts } from "../../actions/postActions";
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
   render() {
+    console.log(this.props);
+    const { posts, loading } = this.props.post;
+
+    let postContent;
+
+    if (posts == null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -20,7 +36,16 @@ class Posts extends Component {
   }
 }
 
-Posts.propTypes = {};
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
 
-const mapStateToProps = state => {};
-export default connect(mapStateToProps)(Posts);
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
